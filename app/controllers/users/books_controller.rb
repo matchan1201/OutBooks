e4f3a0686af27a1cd8e6154cb86aa6d97fcebace
matchan1201 <bumpakano.1@gmail.com>
2020-09-29 11:24:51 +0900

    09/29book編集削除

--- app/controllers/users/books_controller.rb
+++ app/controllers/users/books_controller.rb
@@ -1,13 +1,31 @@
 class Users::BooksController < ApplicationController
 
   def index
+    @books = Book.all
+  end
+  
+  def new
     @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
   end
 
   def create
     @book = Book.new(books_params)
     @book.user_id = current_user.id
     @book.save
+    redirect_to user_path(@book.user_id)
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
     redirect_to user_path(@book.user_id)
   end
 
--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -3,4 +3,8 @@ class Users::UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
   end
+
+  def index
+    @users = User.all
+  end
 end
