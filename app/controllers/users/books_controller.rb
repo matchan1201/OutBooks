8f7e73938261456342c1518a66b2b34aca75a1d6
matchan1201 <bumpakano.1@gmail.com>
2020-10-01 10:45:07 +0900

    10/01

--- app/controllers/users/books_controller.rb
+++ app/controllers/users/books_controller.rb
@@ -20,7 +20,13 @@ class Users::BooksController < ApplicationController
     @book = Book.new(books_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to user_path(@book.user_id)
+    redirect_to book_path(@book)
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(books_params)
+    redirect_to book_path(@book)
   end
 
   def destroy
