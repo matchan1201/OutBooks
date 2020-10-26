8bdae031fa59ca6e1f838ecdd8b2e2ea13946594
matchan1201 <bumpakano.1@gmail.com>
2020-10-26 15:51:54 +0900

    twitterログイン

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable, :omniauthable
 
         has_many :books
         has_many :book_comments, dependent: :destroy
@@ -41,4 +41,28 @@ class User < ApplicationRecord
               @users = User.all
             end
         end
+
+        def self.find_for_oauth(auth)
+          user = User.where(uid: auth.uid, provider: auth.provider).first
+       
+          unless user
+            user = User.create(
+              uid:      auth.uid,
+              provider: auth.provider,
+              email:    User.dummy_email(auth),
+              password: Devise.friendly_token[0, 20],
+              profile_image: auth.info.image,
+              name: auth.info.name,
+              nickname: auth.info.nickname,
+              location: auth.info.location
+            )
+          end
+       
+          user
+        end
+
+  private
+  def self.dummy_email(auth)
+    "#{auth.uid}-#{auth.provider}@example.com"
+  end
 end
